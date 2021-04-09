794cd05178efb26b031c0253b7c0c2e3426df7bc
HenryAndrew356 <58161132+HenryAndrew356@users.noreply.github.com>
2021-04-09 03:01:01 -0500

    Create HeaderOfScitalaVº2.h

---
+++ HeaderOfScitalaVº2.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+class ScitalaHeader
+{
+    public:
+        string mensaje;
+        void encriptarScitala();
+        void desencriptarScitala();
+        void imprimir();
+};
