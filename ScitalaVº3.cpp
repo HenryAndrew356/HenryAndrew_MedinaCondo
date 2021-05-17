afd586afdf4c5322370fec1bfd5731cb31070e52
HenryAndrew356 <58161132+HenryAndrew356@users.noreply.github.com>
2021-05-16 20:32:38 -0500

    Create ScitalaVº3.cpp

---
+++ ScitalaVº3.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
+
+void cifradoScitala(string &mensaje)
+{
+    string mensajePivote=mensaje;
+    int columnas,filas;
+    
+    cout<<"Ingrese las columnas y filas: ";
+    cin>>columnas>>filas;
+    int resto=columnas*filas-mensaje.size();
+    for(int a{0};a<resto;a++)
+    {
+        mensajePivote+=" ";
+        mensaje+=" ";
+    }
+    int pasada{0};
+    int multiplicador{0};
+    for(unsigned int pasadaF{0};pasadaF<columnas;pasadaF++)
+    {
+        multiplicador=0;
+        for(unsigned int pasadaC{pasadaF};pasadaC<filas+pasadaF;pasadaC++)
+        {
+            mensaje[pasada]=mensajePivote[pasadaF+columnas*multiplicador];
+            pasada++;
+            multiplicador++;
+        }
+    }
+}
+
+void descifradoScitala(string &mensaje)
+{
+    string mensajePivote=mensaje;
+    int columnas,filas;
+    cout<<"Ingrese las columnas y filas para descrifrar: ";
+    cin>>columnas>>filas;
+    int pasada{0};
+    int multiplicador{0};
+    for(unsigned int pasadaF{0};pasadaF<filas;pasadaF++)
+    {
+        multiplicador=0;
+        for(unsigned int pasadaC{pasadaF};pasadaC<columnas+pasadaF;pasadaC++)
+        {
+            mensaje[pasada]=mensajePivote[pasadaF+filas*multiplicador];
+            pasada++;
+            multiplicador++;
+        }
+    }
+}
+
+
+int main()
+{
+    string mensaje;
+    cout<<"Ingrese el mensaje:"<<endl;
+    getline(cin,mensaje);
+    descifradoScitala(mensaje);
+    cout<<"El mensaje es: "<<endl<<mensaje;
+    cout<<endl;
+
+    string mensajeAdescifrar;
+    cout<<"Ingrese el mensaje a descifrar:"<<endl;
+    getline(cin,mensajeAdescifrar);
+    descifradoScitala(mensajeAdescifrar);
+    cout<<"El mensaje es: "<<endl<<mensajeAdescifrar;
+    cout<<endl;
+
+    return 0;
+}
